5af12896c43784236214001348a550bf732bef54
Adeoti Nurudeen <adeoti360@gmail.com>
2023-03-08 20:49:12 +0100

    Added a few animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,23 @@
 import heroimg from '../images/hero.png';
 import Resume from './DevAdeotiResume.pdf';
+import {motion as m} from 'framer-motion';
 const Hero = () => {
     return(
         <header className="header" id="hero">
             <div className="bridge">
                 <div className="hero">
                     <section className="hero__intro">
-                        <h3>Hey,  <br/>I'm <span className="linemark">Adeoti</span> Nurudeen.</h3>
+                        <m.h3
+                        initial={{x:-100, y:-200, scale:0.4}}
+                        animate={{x:0, y:0, scale:1}}
+                        transition={{duration:1}}>Hey,  <br/>I'm <span className="linemark">Adeoti</span> Nurudeen.</m.h3>
                         
-                        <p>
+                        <m.p
+                        initial={{x:400}}
+                        animate={{x:0}}
+                        transition={{duration:1.2}}>
                             A Frontend Developer with over 9 years of coding and design experience.
-                        </p><br/><br/>
+                        </m.p><br/><br/>
                         <a href={Resume} className="cta">&hearts; Resume</a>
                         &nbsp;
                         
