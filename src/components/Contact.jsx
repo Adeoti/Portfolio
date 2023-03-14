1d34a271924de92526ee437e43e3843406186feb
Adeoti <adeoti360@gmail.com>
2023-03-14 08:18:06 +0100

    Emailjs new import

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import {useRef, useState} from 'react';
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
     const Contact = () => {
     const [emailsent, setEmailsent] = useState(false);
