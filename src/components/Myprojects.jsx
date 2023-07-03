0fe7a910a2af5a632516ca53b131580b64dfca9c
Adeoti Nurudeen <adeoti360@gmail.com>
2023-07-03 08:05:43 +0100

    Added the WP plugins

--- src/components/Myprojects.jsx
+++ src/components/Myprojects.jsx
@@ -23,12 +23,14 @@ const  MyProjects  = () => {
                 </div>
                 
                 <div className="projects__cards">
-                    
+                
+                    <Projects sourcecode="yes" title="WhatsAppify Plugin" image={qsaleswebsite} subtitle="A WordPress Plugin that facilitates single and multiple agent customer support for a business" desc="I built this open source WP plugin to help companies boost their conversion rate, by enabling easy WhatsApp chat features on their website." arran="left2right" linkcode="https://github.com/Adeoti/whatsappify" linklive="https://getqsales.com/"/>
+                    <Projects sourcecode="yes" title="ScriptPress Plugin" image={qsaleswebsite} subtitle="A Fast WordPress Plugin to insert custom scripts to wordpress sites" desc="I coded and released this Wp plugin in order to allow developers to safely insert custom css and javascript codes to their WordPress websites." arran="right2left" linkcode="https://github.com/Adeoti/scriptpress" linklive="https://getqsales.com/"/>
                     <Projects sourcecode="yes" title="Q-Sales Official Website" image={qsaleswebsite} subtitle="The official Website of Q-Sales" desc="I got hired in 2022 to re-create a responsive website for Q-Sales Technology. And I built it with REACT and SASS" arran="left2right" linkcode="https://github.com/Adeoti/qsaleswebsite" linklive="https://getqsales.com/"/>
                     <Projects sourcecode="no" title="ASDI International Site" image={asdiimg} subtitle="The official website of a global organization" desc="Back in 2021, I got hired as a freelancer to build the official website  of this organization. I built the site using WORDPRESS, PHOTOSHOP and CSS." arran="right2left" linkcode="" linklive="https://asdi-international.org/"/>
                     <Projects sourcecode="yes" title="QR-Code Hatch" image={qrcodeimage} subtitle="A free web app to generate custom QR codes" desc="I built this open source web app that generates QR-Codes on the fly; without having to sign up or login. I built the app with HTML, CSS, JQUERY, ETC." arran="left2right" linkcode="https://github.com/Adeoti/Qrcode-hatch" linklive="https://www.qrcode-hatch.com/"/>
-                    <Projects sourcecode="yes" title="Q-Sales Inventory App" image={qsalesapp} subtitle="A web-based inventory application" desc="I was hired to design the interface and some key functionalities of the inventory application; from the main dashboard, to all various admin pages. I used HTML5, CSS3, jQUERY, Bootstrap, PHP, and MySQL." arran="right2left" linkcode="https://github.com/Adeoti/qsalesinventoryapp" linklive="https://github.com/Adeoti/qsalesinventoryapp"/> 
-                    <Projects sourcecode="yes" title="DicIt" image={dicitimg} subtitle="A full-functioning dictionary web app" desc="I created this app with react, and styled it with CSS3, using the CSS BEM methodology. I then powered its functionality with the open source dictionary API." arran="left2right" linkcode="https://github.com/Adeoti/dictionary" linklive="https://dictit.netlify.app/"/>  
+                  
+                    <Projects sourcecode="yes" title="DicIt" image={dicitimg} subtitle="A full-functioning dictionary web app" desc="I created this app with react, and styled it with CSS3, using the CSS BEM methodology. I then powered its functionality with the open source dictionary API." arran="right2left" linkcode="https://github.com/Adeoti/dictionary" linklive="https://dictit.netlify.app/"/>  
                 
                 </div>
           
